891a79de2970fdd421f864574e1f7475e1c110c7
itthinx <itthinx@gmail.com>
2017-10-18 21:45:58 +0200

    copyright corrected; updated plugin description

--- affiliates-ninja-forms.php
+++ affiliates-ninja-forms.php
@@ -2,7 +2,7 @@
 /**
  * affiliates-ninja-forms.php
  *
- * Copyright (c) 2012-2015 "kento" Karim Rahimpur www.itthinx.com
+ * Copyright (c) 2017 "kento" Karim Rahimpur www.itthinx.com
  *
  * This code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt.
@@ -20,7 +20,7 @@
  *
  * Plugin Name: Affiliates Ninja Forms Integration
  * Plugin URI: http://www.itthinx.com/plugins/affiliates-ninja-forms/
- * Description: Integrates Affiliates with Ninja Forms
+ * Description: Integrates <a href="https://wordpress.org/plugins/affiliates/">Affiliates</a>, <a href="https://www.itthinx.com/shop/affiliates-pro/">Affiliates Pro</a> and <a href="https://www.itthinx.com/shop/affiliates-enterprise/">Affiliates Enterprise</a> with <a href="https://wordpress.org/plugins/ninja-forms/">Ninja Forms</a>.
  * Version: 2.0.0
  * Author: itthinx
  * Author URI: http://www.itthinx.com/
