098b06fdd2b7467f690fd0da6b176129d01eaf5f
itthinx <itthinx@gmail.com>
2017-10-23 11:20:14 +0200

    license notes reviewed for GPL

--- lib/class-affiliates-nf-admin.php
+++ lib/class-affiliates-nf-admin.php
@@ -2,15 +2,15 @@
 /**
  * class-affiliates-nf-settings.php
  *
- * Copyright (c) 2014 - 2017 www.itthinx.com
+ * Copyright (c) "kento" Karim Rahimpur www.itthinx.com
  *
- * This code is provided subject to the license granted.
- * Unauthorized use and distribution is prohibited.
- * See COPYRIGHT.txt and LICENSE.txt
+ * This code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
  *
  * This code is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * This header and all notices must be kept intact.
  *
